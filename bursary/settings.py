e934002c2124ac3924b822eef78581b36e512192
Steven8119 <stevenwaithaka8119@gmail.com>
2023-12-21 14:02:16 +0300

    changes in sttngs.py

--- bursary/settings.py
+++ bursary/settings.py
@@ -92,13 +92,13 @@ WSGI_APPLICATION = "bursary.wsgi.application"
 #             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
 #    },
 #     }
+# # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
